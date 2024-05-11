c953177685e354a8b6d061d59d11888f8437e353
Adam <adam.nemmish213@gmail.com>
2024-05-11 22:21:46 +0600

    Создал модель гостевой книги, зарегал в админке, так же создал файл с фикстурами

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import GuestBook
 
-# Register your models here.
+
+@admin.register(GuestBook)
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author_name', 'created_at']
+    list_filter = ['author_name']
+    search_fields = ['id', 'author_name']
+    fields = ['author_name', 'author_gmail', 'text', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+status_field = [("active", "Активно"), ("blocked", "Заблокировано")]
+
+
+class GuestBook(models.Model):
+    author_name = models.CharField(max_length=50, verbose_name='Имя автора')
+    author_gmail = models.EmailField(verbose_name='Почта автора', max_length=200)
+    text = models.TextField(max_length=100, verbose_name='Текст записи')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+    status = models.CharField(verbose_name='Статус', default='active', choices=status_field, max_length=50)
+
+    def __str__(self):
+        return f'{self.author_name} {self.status}'
