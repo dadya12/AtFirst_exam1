15649b29c3debdaa5ebf2f2756b84580589a2d74
Adam <adam.nemmish213@gmail.com>
2024-05-11 23:05:43 +0600

    Выполнил 2 этап

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from webapp.views import home_page
+
 
 urlpatterns = [
-
+    path('', home_page, name='home_page'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from webapp.models import GuestBook
 
-# Create your views here.
+
+def home_page(request):
+    book = GuestBook.objects.all().filter(status='active').order_by('-created_at')
+    context = {'book': book}
+    return render(request, 'home_page.html', context)
